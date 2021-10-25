9be63115a7b957f4e27c03502b3c019dec1d5ffa
kkvale <marcocaccavale.napoletano@gmail.com>
2021-10-25 14:41:38 +0200

    the beginning of the end

---
+++ 00 - ProjectWorkTest1/Models/Prodotti.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _00___ProjectWorkTest1.Models
+{
+    public class Prodotti
+    {
+    }
+}
