7e4d395024d8e81dbe58deee397bcc925e2e4e9e
kkvale <marcocaccavale.napoletano@gmail.com>
2021-10-25 14:39:59 +0200

    Aggiungere i file di progetto.

---
+++ 00 - ProjectWorkTest1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _00___ProjectWorkTest1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
